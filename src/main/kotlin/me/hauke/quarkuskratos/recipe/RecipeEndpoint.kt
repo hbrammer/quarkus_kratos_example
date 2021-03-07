4e87762274ea9cdb14b72b2d09781296458514f1
Hauke Brammer <hauke.brammer@googlemail.com>
2021-03-07 12:14:12 +0100

    Added recipe endpoint

---
+++ src/main/kotlin/me/hauke/quarkuskratos/recipe/RecipeEndpoint.kt
@@ -0,0 +1,20 @@
+package me.hauke.quarkuskratos.recipe
+
+import io.quarkus.qute.Template
+import io.quarkus.qute.TemplateInstance
+import io.quarkus.qute.api.ResourcePath
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/recipe")
+class RecipeEndpoint @Inject constructor(
+        @ResourcePath("recipe/recipe.html") val recipe: Template
+) {
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    fun get(): TemplateInstance =
+            recipe.instance()
+}
